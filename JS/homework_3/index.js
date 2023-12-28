0dcd9793857835f2a33e94a0fac6a466332be741
Desento <astapenko91@gmail.com>
2023-12-28 19:44:23 +0100

    task for homework3 index.js

---
+++ JS/homework_3/index.js
@@ -0,0 +1,32 @@
+Array.prototype.myFilter = function (callback, thisArg) {
+
+    if (typeof callback !== "function") {
+        throw new Error("Callback is not a function");
+    }
+
+    const objectFromArray = Object(this);
+    const result = [];
+
+    for (let i = 0; i < objectFromArray.length; i++) {
+
+        if (i in objectFromArray) {
+
+            if (callback.call(thisArg, objectFromArray[i], i, objectFromArray)) {
+                result.push(objectFromArray[i]);
+            }
+        }
+    }
+
+    return result;
+};
+
+const createDebounceFunction = (func, wait) => {
+    let timeout;
+
+    return function (...args) {
+        const context = this;
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(context, args), wait);
+    };
+};
